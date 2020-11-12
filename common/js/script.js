d3503d667368fbbc4aa8993c8c145277af411a6f
NaotoIijima <mendokotearchery@gmail.com>
2020-11-12 15:30:47 +0900

    Update script.js

--- common/js/script.js
+++ common/js/script.js
@@ -8,11 +8,11 @@ addEventListener('mousemove', {sample, handleEvent: direction},false);
 function direction(e) {
     var camera = document.getElementById('camera');
     var rotation = camera.getAttribute('rotation');
-    //var rotation = document.querySelector('a-scene').camera.el.object3D.rotation;
-    //document.write(rotation.y);
-    //sample.crossfade(50);
-    console.log(rotation.y);
-    this.sample.crossfade(rotation.y);
-    //if(rotation.y<0) document.write(rotation.y);
+    var position = document.getElementById('pos');
+    console.log(rotation.y); 
+    this.sample.crossfade(rotation.y,position.value);
+
 }
+
+
     
